517ac0e4cad91b330eb6b2048897a48f157f322e
Mayank Verma <mayank.verma@dish.com>
2024-08-02 23:26:31 +0530

    Added Sum of triplet in an array code

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"fmt"
 	t "github.com/craftizmv/DS/pkg/twopointers"
 )
 
 func main() {
-	tp := t.NewTP()
-	//input := []int16{1, 2, 3, 4, 5}
-	//rA := tp.ReverseArray(input)
-	//fmt.Printf("val is +%v", rA)
+	//tp := t.NewTP()
+	////input := []int16{1, 2, 3, 4, 5}
+	////rA := tp.ReverseArray(input)
+	////fmt.Printf("val is +%v", rA)
+	//
+	//input2 := []int{1, 2, 3, 4, 5}
+	//result := tp.FindSumOfThree(input2, 4)
+	//println("Sum Of Three Result", result)
 
-	input2 := []int{1, 2, 3, 4, 5}
-	result := tp.FindSumOfThree(input2, 9)
-	println("Sum Of Three Result", result)
-
+	arr := []int{738, 286, 814, 742, 910, -551, -500, 22, -794, -639, -167, -555, -117, -978, -741, -373, -999, 455, 248, 321, -569, -228, -969, -409, -251, 98, -54, 114, -897, -523, 554, -372}
+	if t.HasTripletWithTargetSum(arr, 0) {
+		fmt.Println("There is a triplet with zero sum.")
+	} else {
+		fmt.Println("There is no triplet with zero sum.")
+	}
 }
---
+++ pkg/twopointers/sum_of_three_gpt.go
@@ -0,0 +1,31 @@
+package twopointers
+
+import (
+	"sort"
+)
+
+func HasTripletWithTargetSum(arr []int, target int) bool {
+	// Step 1: Sort the array
+	sort.Ints(arr)
+
+	// Step 2: Iterate through the array
+	for i := 0; i < len(arr)-2; i++ {
+		// Initialize two pointers
+		left := i + 1
+		right := len(arr) - 1
+
+		// Use two pointers to find a pair with the sum equal to target - arr[i]
+		for left < right {
+			sum := arr[i] + arr[left] + arr[right]
+			if sum == target {
+				return true
+			} else if sum < target {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+
+	return false
+}
