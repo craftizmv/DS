c5debc161860fbd76f0bcb3ed400ce409f72e5b2
Mayank Verma <mayank.verma@dish.com>
2024-11-29 10:20:42 +0530

    Added 3sum optimised logic with sorting and TP(two pointer)

--- pkg/arrays/three_sum.go
+++ pkg/arrays/three_sum.go
@@ -62,51 +62,52 @@ func ThreeSumBruteForceTriplet(input []int, target int) ([][]int, error) {
 	return result, nil
 }
 
-// ThreeSumOptimisedTwoPointer : efficient way to create 3 sum.
-// 1. sort and then move the 2 pointers, maintain the target in hashmap.
-func ThreeSumOptimisedTwoPointer(input []int, target int) bool {
-	// check the input
+func ThreeSumTwoPointerTriplets(input []int, target int) [][]int {
+	var result [][]int
 	if len(input) < 3 {
-		return false
+		return result
 	}
 
-	// sort the input array
 	sort.Ints(input)
 
-	// follow 2p approach for looking at the target
-	complementLookupMap := make(map[int]struct{}, len(input)/2)
-	start, end := 0, len(input)-1
-	moveForward := 0
-	for start < end {
-		startVal := input[start]
-		if moveForward == 1 {
-			// check if the target is present in the map or not.
-			if _, ok := complementLookupMap[startVal]; ok {
-				return true
-			}
-		}
-		endVal := input[end]
-		if moveForward == -1 {
-			if _, ok := complementLookupMap[endVal]; ok {
-				return true
-			}
-		}
+	// we will navigate the outer loop until the n-2
+	// we will run the inner loop - while l < r
+	for i := 0; i < len(input)-2; i++ {
+		x := input[i]
+		l := i + 1
+		r := len(input) - 1
 
-		complement := target - (startVal + endVal)
-		complementLookupMap[complement] = struct{}{}
-
-		// increment the 2p vars based on the target val
-		if complement > target {
-			start++
-			moveForward = 1
-		} else {
-			end--
-			moveForward = -1
+		if i > 0 && input[i] == input[i-1] {
+			continue
+		}
+		for l < r {
+			y := input[l]
+			z := input[r]
+			currSum := x + y + z
+			if currSum == target {
+				result = append(result, []int{x, y, z})
+				// Skip consecutive duplicates for `left` and `right`
+				for l < r && input[l] == input[l+1] {
+					l++
+				}
+				for l < r && input[r] == input[r-1] {
+					r--
+				}
+				l++
+				r--
+			} else if target > 0 {
+				r--
+			} else {
+				l++
+			}
 		}
 	}
 
-	return false
+	return result
 }
+
+// ThreeSumOptimisedTwoPointer : efficient way to create 3 sum.
+// 1. sort and then move the 2 pointers, maintain the target in hashmap.
 
 // ThreeSumOptimisedTwoLoops : Could be using 2 loops and one hashmap we search the target.
 func ThreeSumOptimisedTwoLoops(input []int, target int) ([][]int, error) {
