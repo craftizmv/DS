e393a4b6ee4e16bcb9a5814808de3f4759c7b37f
Mayank Verma <mayank.verma12389@gmail.com>
2026-01-09 00:16:59 +0530

    Added isBalanced binary tree func.

--- cmd/dsa-trees/main.go
+++ cmd/dsa-trees/main.go
@@ -8,36 +8,36 @@ import (
 
 // morris traversal
 func main() {
-	root := &binary_tree.Node{
-		Data: 1,
-	}
-	root.Left = &binary_tree.Node{
-		Data: 2,
-	}
-	root.Right = &binary_tree.Node{
-		Data: 6,
-	}
-	root.Right.Left = &binary_tree.Node{
-		Data: 9,
-	}
-	root.Right.Right = &binary_tree.Node{
-		Data: 7,
-	}
+	// root := &binary_tree.Node{
+	// 	Data: 1,
+	// }
+	// root.Left = &binary_tree.Node{
+	// 	Data: 2,
+	// }
+	// root.Right = &binary_tree.Node{
+	// 	Data: 6,
+	// }
+	// root.Right.Left = &binary_tree.Node{
+	// 	Data: 9,
+	// }
+	// root.Right.Right = &binary_tree.Node{
+	// 	Data: 7,
+	// }
 
-	root.Left.Left = &binary_tree.Node{
-		Data: 3,
-	}
-	root.Left.Right = &binary_tree.Node{
-		Data: 4,
-	}
-	root.Left.Right.Left = &binary_tree.Node{
-		Data: 8,
-	}
-	root.Left.Right.Right = &binary_tree.Node{
-		Data: 5,
-	}
+	// root.Left.Left = &binary_tree.Node{
+	// 	Data: 3,
+	// }
+	// root.Left.Right = &binary_tree.Node{
+	// 	Data: 4,
+	// }
+	// root.Left.Right.Left = &binary_tree.Node{
+	// 	Data: 8,
+	// }
+	// root.Left.Right.Right = &binary_tree.Node{
+	// 	Data: 5,
+	// }
 
-	bt := binary_tree.NewBinaryTree(root)
+	// bt := binary_tree.NewBinaryTree(root)
 	//err := bt.MorrisInOrderTraversal(root)
 	//if err != nil {
 	//	fmt.Println(err)
@@ -58,6 +58,44 @@ func main() {
 
 	// bt.DFSPostOrderOneStack(root)
 
-	fmt.Println("Depth is ", bt.MaxDepth(root))
+	// fmt.Println("Depth is ", bt.MaxDepth(root))
 
+	skewTree, root := sampleSkewTree()
+	fmt.Println("Is Balanced ", skewTree.IsBalancedTree(root))
+
+}
+
+func sampleSkewTree() (*binary_tree.BinaryTree, *binary_tree.Node) {
+
+	root := &binary_tree.Node{
+		Data: 1,
+	}
+	root.Left = &binary_tree.Node{
+		Data: 2,
+	}
+	root.Left.Left = &binary_tree.Node{
+		Data: 6,
+	}
+	root.Left.Left.Left = &binary_tree.Node{
+		Data: 9,
+	}
+	root.Left.Left.Left.Right = &binary_tree.Node{
+		Data: 7,
+	}
+
+	root.Left.Left = &binary_tree.Node{
+		Data: 3,
+	}
+	root.Left.Right = &binary_tree.Node{
+		Data: 4,
+	}
+	root.Left.Right.Left = &binary_tree.Node{
+		Data: 8,
+	}
+	root.Left.Right.Right = &binary_tree.Node{
+		Data: 5,
+	}
+
+	bt := binary_tree.NewBinaryTree(root)
+	return bt, root
 }
--- pkg/tree/binary-tree/bin_tree.go
+++ pkg/tree/binary-tree/bin_tree.go
@@ -3,6 +3,7 @@ package binary_tree
 import (
 	"errors"
 	"fmt"
+	"math"
 
 	"github.com/lossdev/stack"
 )
@@ -309,4 +310,32 @@ func (bt *BinaryTree) MaxDepth(root *Node) int {
 	}
 
 	return 1 + max(bt.MaxDepth(root.Left), bt.MaxDepth(root.Right))
+}
+
+// IsBalancedTree - Checks if the tree is balanced or not.
+// TC : O(N)
+func (bt *BinaryTree) IsBalancedTree(root *Node) bool {
+	return bt.isBalanced(root) != -1
+}
+
+func (bt *BinaryTree) isBalanced(root *Node) int {
+	if root == nil {
+		return 0
+	}
+
+	leftHeight := 1 + bt.isBalanced(root.Left)
+	rightHeight := 1 + bt.isBalanced(root.Right)
+
+	if leftHeight == -1 || rightHeight == -1 {
+		// it means that either the left or the right subtree
+		// is not balanced.
+		return -1
+	}
+
+	// if abs difference is greater than 1 then return -1
+	if math.Abs(float64(rightHeight-leftHeight)) > 1 {
+		return -1
+	}
+
+	return max(leftHeight, rightHeight)
 }
